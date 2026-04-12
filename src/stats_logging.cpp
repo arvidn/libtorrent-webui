cba9d16c39e73eae9737c8e1a1d08f4b75630e80
Arvid Norberg <arvid@libtorrent.org>
2026-04-12 13:07:14 +0200

    update stats_logger to be a bit more modern and not use a deprecated libtorrent alert

--- src/stats_logging.cpp
+++ src/stats_logging.cpp
@@ -8,7 +8,8 @@ see LICENSE file.
 */
 
 #include <functional>
-#include <cinttypes>
+#include <iomanip>
+#include <cstdio>
 
 #include "stats_logging.hpp"
 #include "libtorrent/session.hpp"
@@ -24,12 +25,10 @@ using namespace std::placeholders;
 stats_logging::stats_logging(lt::session& s, alert_handler* h)
 	: m_alerts(h)
 	, m_ses(s)
-	, m_stats_logger(NULL)
 	, m_log_seq(0)
 {
 	m_alerts->subscribe(this, 0
 		, lt::session_stats_alert::alert_type
-		, lt::stats_alert::alert_type
 		, 0);
 	rotate_stats_log();
 }
@@ -41,26 +40,29 @@ stats_logging::~stats_logging()
 
 void stats_logging::rotate_stats_log()
 {
-	if (m_stats_logger)
+	if (m_stats_logger.is_open())
 	{
 		++m_log_seq;
-		fclose(m_stats_logger);
+		m_stats_logger.close();
 	}
 
-	char filename[100];
-	{ std::error_code fec; std::filesystem::create_directory("session_stats", fec); }
+	{
+		std::error_code fec;
+		std::filesystem::create_directory("session_stats", fec);
+		// TODO: report errors
+	}
 #ifdef TORRENT_WINDOWS
 	const int pid = GetCurrentProcessId();
 #else
 	const int pid = getpid();
 #endif
+	char filename[100];
 	snprintf(filename, sizeof(filename), "session_stats/%d.%04d.log", pid, m_log_seq);
-	m_stats_logger = fopen(filename, "w+");
+	m_stats_logger.open(filename, std::ios::out | std::ios::trunc);
 	m_last_log_rotation = lt::clock_type::now();
-	if (m_stats_logger == 0)
+	if (!m_stats_logger)
 	{
-		fprintf(stderr, "Failed to create lt::session stats log file \"%s\": (%d) %s\n"
-			, filename, errno, strerror(errno));
+		std::fprintf(stderr, "Failed to create lt::session stats log file \"%s\"\n", filename);
 		return;
 	}
 
@@ -70,40 +72,39 @@ void stats_logging::rotate_stats_log()
 		{ return lhs.value_index < rhs.value_index; });
 
 	int idx = 0;
-	fputs("second", m_stats_logger);
+	m_stats_logger << "second";
 	for (auto const c : cnts)
 	{
 		// just in case there are some indices that don't have names
 		// (it shouldn't really happen)
 		while (idx < c.value_index)
 		{
-			fprintf(m_stats_logger, ":");
+			m_stats_logger << ':';
 			++idx;
 		}
 
-		fprintf(m_stats_logger, ":%s", c.name);
+		m_stats_logger << ':' << c.name;
 		++idx;
 	}
-	fputs("\n\n", m_stats_logger);
+	m_stats_logger << "\n\n";
 }
 
 void stats_logging::handle_alert(lt::alert const* a)
 {
 	lt::session_stats_alert const* s = lt::alert_cast<lt::session_stats_alert>(a);
-
-	if (s == NULL)
-	{
-		m_ses.post_session_stats();
-		return;
-	}
+	if (s == nullptr) return;
 
 	if (lt::clock_type::now() - m_last_log_rotation > lt::hours(1))
 		rotate_stats_log();
 
-	fprintf(m_stats_logger, "%f", double(lt::total_microseconds(s->timestamp() - m_last_log_rotation)) / 1000000.0);
+	if (!m_stats_logger)
+		return;
+
+	m_stats_logger << std::fixed << std::setprecision(6)
+		<< double(lt::total_microseconds(s->timestamp() - m_last_log_rotation)) / 1000000.0;
 	for (auto const v : s->counters())
-		fprintf(m_stats_logger, "\t%" PRId64, v);
-	fprintf(m_stats_logger, "\n");
+		m_stats_logger << '\t' << v;
+	m_stats_logger << '\n';
 }
 
 }
